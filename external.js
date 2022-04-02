303371c4b69898100fb34f988b9128e2731ddf6d
justvietnh <vietnh95@gmail.com>
2022-04-02 11:25:41 +0200

    Writing singleGame()

--- external.js
+++ external.js
@@ -6,5 +6,39 @@ function computerPlay() {
     return item
 }
 
-console.log(computerPlay());
+function singleGame(playerSel, computerSel) {
+    // playerSel and computerSel are either Rock, Paper or Scissors
+    let playerSelect = playerSel.toLowerCase().charAt(0).toUpperCase() + playerSel.toLowerCase().slice(1);
+    let comSelect = computerSel.toLowerCase().charAt(0).toUpperCase() + computerSel.toLowerCase().slice(1);
+
+    // if tie just get out of func
+    if (playerSel == computerSel) {
+        return "Tie Game! You chose " + playerSel + " and the computer as well!"
+    }
+
+    if (playerSelect == "Rock") {
+        if (comSelect == "Paper") {
+            return "You lose! " + comSelect + " beats " + playerSelect
+        } else if (comSelect == "Scissors") {
+            return "You win! "  + playerSelect + " beats " + comSelect
+        }
+    } else if (playerSelect == "Paper") {
+        if (comSelect == "Scissors") {
+            return "You lose! " + comSelect + " beats " + playerSelect
+        } else if (comSelect == "Rock") {
+            return "You win! "  + playerSelect + " beats " + comSelect
+        }
+    } else if (playerSelect == "Scissors") {
+        if (comSelect == "Rock") {
+            return "You lose! " + comSelect + " beats " + playerSelect
+        } else if (comSelect == "Paper") {
+            return "You win! "  + playerSelect + " beats " + comSelect
+        }
+    }
+    }
+
+let player ="Rock";
+let com = computerPlay()
+
+console.log(singleGame(player, com));
 
